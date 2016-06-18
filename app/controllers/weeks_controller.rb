6568573aea4f3248f51a82596232ad8ce03774df
Harsha Murthy <Harsha@Harshas-MacBook-Pro.local>
2016-06-18 12:46:18 -0500

    Remove binding.pry

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -1,7 +1,6 @@
 class WeeksController < ApplicationController
   def show
     @birthday = params[:request][:intent][:slots]["Birthday"][:value]
-    binding.pry
     render json: @birthday
   end
 end
