c35d27694f2836f74eaac4aab1c8d33511438bd9
Harsha Murthy <Harsha@Harshas-MacBook-Pro.local>
2016-06-18 13:01:18 -0500

    Add skip_before_action to controller

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -1,4 +1,6 @@
 class WeeksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def show
     @birthday = params[:request][:intent][:slots]["Birthday"][:value]
     render json: @birthday
