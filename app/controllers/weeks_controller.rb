a1e1ceb4866599127fdb54cee13c8d1eb782a3f4
Harsha Murthy <Harsha@Harshas-MacBook-Pro.local>
2016-06-18 12:44:04 -0500

    Add pry to the gemfile

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -1,6 +1,7 @@
 class WeeksController < ApplicationController
   def show
     @birthday = params[:request][:intent][:slots]["Birthday"][:value]
+    binding.pry
     render json: @birthday
   end
 end
