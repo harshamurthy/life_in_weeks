3ecd3f3cf982919b6563e1c294e3eba78274145b
Harsha Murthy <Harsha@Harshas-MacBook-Pro.local>
2016-06-18 13:12:42 -0500

    Calculate weeks left

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -1,8 +1,11 @@
 class WeeksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def show
+  def create
     @birthday = params[:request][:intent][:slots]["Birthday"][:value]
-    render json: @birthday
+    weeks_lived = ((Date.today - Date.parse(@birthday)).to_i)/7
+    weeks_in_ninety_years = 4692
+    weeks_left = weeks_in_ninety_years - weeks_lived
+    render json: weeks_left
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  post '/getweeks' => 'weeks#show'
+  post '/weeksleft' => 'weeks#create'
 end
